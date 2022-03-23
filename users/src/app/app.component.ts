a3f3ad91b2bc7ef6d689f641a922ab63105d3274
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-23 10:03:59 -0600

    📅  | Wed Mar 23 10:03:59 MDT 2022

--- users/src/app/app.component.ts
+++ users/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Store, select } from '@ngrx/store';
 import { Component } from '@angular/core';
 import { selectAll, State } from './user.reducer';
 import { map } from 'rxjs/operators';
+import { User } from './user.model';
+import { addUser } from './user.actions';
 
 @Component({
   selector: 'app-root',
@@ -10,14 +12,25 @@ import { map } from 'rxjs/operators';
 })
 export class AppComponent {
   title = 'users';
+  id = 1;
   users$;
+  user: any;
   constructor(private store: Store<State>) {
     this.users$ = this.store.select((state) => state).pipe(map(this.toArray));
 
     this.users$.subscribe((data) => console.log('users', data));
   }
   toArray(ob: any) {
-    const keys = Object.keys(ob);
-    return keys.map((key) => ob[key]);
+    const {
+      users: { entities },
+    } = ob;
+    console.log(entities);
+    const keys = Object.keys(entities);
+    return keys.map((key) => entities[key]);
+  }
+
+  add() {
+    const newUser = { id: this.id++, name: this.user };
+    this.store.dispatch(addUser({ user: newUser }));
   }
 }
